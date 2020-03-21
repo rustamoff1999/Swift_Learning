c11e7002c98d13ef416f31786f1804348a6e48f4
fuadrustamov999 <56446556+fuadrustamov999@users.noreply.github.com>
2020-03-21 22:22:32 +0400

    Added Optional coalescing and Optional binding

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -22,6 +22,7 @@ myPhone1
 //Извлечение опционального значения
 typealias num = Int
 var optional1 : num? = 1800
+type(of:optional1)
 optional1 = optional1! + 200 //: **Принудительное извлечение значения**
 var notOptional = optional1! + 200
 let number1 = optional1!
@@ -29,3 +30,44 @@ let number1 = optional1!
 //:**Косвенное извлечение значения**
 var optional2 : (Int,Int)! = (15,228)
 let variable = optional2
+
+//:**nil coalescing**
+var optionalString : Optional<String> = "It's optional"
+
+let mustHaveResult : String
+mustHaveResult = optionalString ?? ""
+
+//Another example
+
+var optionalInt : Int? = nil
+var itMustHasResult : Int = optionalInt ?? 0
+
+//Another example
+typealias OF = Float?
+var someOptional : OF = 4.81
+let mustHasResult : Float = someOptional ?? 0.0
+
+// /////////////////////////////////////////////////
+//:**Optional binding**
+
+var someOpt : String? = nil
+let result : String
+
+if let unwrapped = someOpt {
+    result = unwrapped
+}
+else {
+    result = ""
+}
+print(result)
+
+var anotherOptional : Int? = 30
+var someResult : Int
+
+if let unwrapped = anotherOptional {
+    someResult = unwrapped
+}
+else {
+    someResult = 0
+}
+print(someResult)
